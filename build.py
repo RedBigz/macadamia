a3da77a6154f0c80e78dbabae496160bd3f64230
RedBigz <signoff@redbigz.com>
2024-05-26 09:50:39 +1000

    Change name to Macadamia and remove obfuscation

--- build.py
+++ build.py
@@ -1,12 +1,16 @@
-import os
+import os, shutil
+
+shutil.rmtree("out")
+
+os.makedirs("out", exist_ok=True)
 
 os.system("tsc")
 
-with open("ccm.js.part", "r") as ccmpart, open("ccm.part2.js", "w+") as ccmfile, open("loader.js", "r") as loader:
-    cpr = ccmpart.read()
-    ccmfile.write(loader.read().replace("/* REPLACE */", cpr.replace("\n", "\n\t").strip() + ("\n\trequire([\"index\"]);" if "define(\"index" in cpr else "")))
+with open("./out/macadamia.js.part", "r") as macadamiapart, open("./out/macadamia.js", "w+") as macadamiafile, open("loader.js", "r") as loader:
+    cpr = macadamiapart.read()
+    macadamiafile.write(loader.read().replace("/* REPLACE */", cpr.replace("\n", "\n\t").strip() + ("\n\trequire([\"index\"]);" if "define(\"index" in cpr else "")))
 
-os.system("javascript-obfuscator.cmd ccm.part2.js --output ccm.js")
+# os.system("javascript-obfuscator.cmd macadamia.part2.js --output macadamia.js")
 
-os.remove("ccm.js.part")
-os.remove("ccm.part2.js")
+os.remove("./out/macadamia.js.part")
+# os.rename("./out/macadamia.js.part.map", "./out/macadamia.js.map")
